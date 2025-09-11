9c055d4d5e2ae441f92924a25c8e3bd8e5730a2c
Bryan Edward <bryanedwardse@gmail.com>
2025-09-11 12:20:20 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import FAB from './components/FAB';
 
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={ styles.textHuge }>{ count }</Text>
+
+      <FAB
+        label='+1'
+        onPress={() => setCount(count + 1)} 
+        onLongPress={() => setCount(0)}
+        />
+        
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +28,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100'
   },
 });
